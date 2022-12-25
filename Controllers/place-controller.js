aba459a1fcb252420fe6c28b57fed650d6f503d9
Hamza Qahoush <hamza.qah@gmail.com>
2022-12-25 14:05:12 +0300

    add controllers

---
+++ Controllers/place-controller.js
@@ -0,0 +1,69 @@
+const HttpError = require("../models/http-error");
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p3",
+    title: "PETRA",
+    description: "One of the most famous sky scrapers in the Jordan!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Emp. State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: "u2",
+  },
+];
+
+const getPlaceByUser = (req, res, next) => {
+  const userPlaces = DUMMY_PLACES.filter((u) => u.creator === req.params.uid);
+  if (userPlaces.length === 0) {
+    return next(
+      new HttpError(`The user of id = ${req.params.uid} has no places`, 404)
+    );
+  }
+
+  res.json({
+    userPlaces,
+  });
+};
+
+const getPlaceById = (req, res, next) => {
+  const place = DUMMY_PLACES.find((p) => p.id === req.params.pid);
+  if (!place) {
+    throw new HttpError(
+      `The are No Places with id = ${req.params.pid} Sorry! `,
+      404
+    );
+  }
+  res.json({ place });
+};
+
+module.exports = { getPlaceById, getPlaceByUser };
---
+++ models/http-error.js
@@ -0,0 +1,7 @@
+class HttpError extends Error {
+  constructor(messege, errorCode) {
+    super(messege);
+    this.code = errorCode;
+  }
+}
+module.exports = HttpError;
